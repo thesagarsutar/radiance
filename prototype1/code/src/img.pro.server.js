c535c4f8264f2e4e60df657a25a60907d9900f7f
thesagarsutar <sagy26.1991@gmail.com>
2015-11-21 05:31:52 +0530

    filters try differnt libraries

--- prototype1/code/src/img.pro.server.js
+++ prototype1/code/src/img.pro.server.js
@@ -61,19 +61,58 @@ var http = require('http').createServer(function (request, response) {
 
 console.log('server is running at http://localhost:7828');
 
-Caman("image.png", function () {
 
-            this.sepia(0);
-            this.exposure(57);
-            this.greyscale();
-            this.noise(0);
-            this.brightness(10);
-            this.contrast(0);
-            this.gamma(2);
-            this.sharpen(100);
-             // this.curves('rgb', [0, 0], [100, 120], [180, 240], [255, 255]);
-            this.render(function () {
-            	console.log('file saved');
-              this.save("./out.png");
-            });
-          });
+// node-canamJs
+Caman("image.png", function () {
+  this.sepia(0);
+  this.exposure(57);
+  this.greyscale();
+  this.noise(0);
+  this.brightness(10);
+  this.contrast(0);
+  this.gamma(2);
+  this.sharpen(100);
+   // this.curves('rgb', [0, 0], [100, 120], [180, 240], [255, 255]);
+  this.render(function () {
+  	console.log('file saved');
+    this.save("./out.png");
+  });
+});
+
+
+//node-lwip
+// obtain an image object:
+// require('lwip').open('image.png', function(err, image){
+
+//   // check err...
+//   // define a batch of manipulations and save to disk as JPEG:
+//   image.batch()
+//     .sharpen(100)
+//     .saturate(-100)
+//     .lighten(1.2)
+//     .writeFile('output.jpg', function(err){
+//       // check err...
+//       // done.
+//     });
+
+// });
+
+
+//node-jimp
+// var Jimp = require("jimp");
+// Jimp.read("image.png").then(function (image) {
+//                 // resize
+//     image.greyscale()                 // set greyscale
+//          .brightness(0.15)
+//          .background(0xFFFFFF)
+//          .color([
+//     { apply: 'lighten', params: [ 20 ] },
+//     { apply: 'brighten', params: [ 10 ] },
+//     { apply: 'greyscale', params: [ 20 ] },
+//     { apply: 'xor', params: [ '#000' ] }
+// ])
+//          .write("output.jpg"); // save
+//     console.info('image created');
+// }).catch(function (err) {
+//     console.error(err);
+// });
